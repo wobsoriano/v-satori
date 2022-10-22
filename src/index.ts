0526c003a7f0acca6bd0188e266a50c10e928222
Robert Soriano <sorianorobertc@gmail.com>
2022-10-22 15:06:09 -0700

    add logic and test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export const one = 1
-export const two = 2
+import { renderToString } from 'vue/server-renderer'
+import { createSSRApp } from 'vue'
+import { html as _html } from 'satori-html'
+import type { Component } from 'vue'
+
+export async function html(component: Component) {
+  const App = createSSRApp(component)
+  const strComponent = await renderToString(App)
+
+  return _html(strComponent)
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,23 @@
+import 'node-fetch-native/polyfill'
 import { describe, expect, it } from 'vitest'
+import { defineComponent } from 'vue'
+import satori from 'satori'
+import { html } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('should run', () => {
+  it('exported', async () => {
+    const App = defineComponent({
+      data: () => ({ width: 300, height: 300, src: 'https://og-image.vercel.app/Hello%20World.png' }),
+      template: '<img :src="src" :width="width" :height="height" />',
+    })
+
+    const res = await html(App)
+    const asd = await satori(res, {
+      width: 300,
+      height: 300,
+      fonts: [],
+    })
+
+    expect(asd).toMatchSnapshot()
   })
 })
