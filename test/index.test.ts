1513974d448dc86bfe22795c749061e068926783
Robert Soriano <sorianorobertc@gmail.com>
2022-10-22 15:10:01 -0700

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,23 +1,21 @@
 import 'node-fetch-native/polyfill'
-import { describe, expect, it } from 'vitest'
+import { expect, test } from 'vitest'
 import { defineComponent } from 'vue'
 import satori from 'satori'
 import { html } from '../src'
 
-describe('should run', () => {
-  it('exported', async () => {
-    const App = defineComponent({
-      data: () => ({ width: 300, height: 300, src: 'https://og-image.vercel.app/Hello%20World.png' }),
-      template: '<img :src="src" :width="width" :height="height" />',
-    })
-
-    const string = await html(App)
-    const svg = await satori(string, {
-      width: 300,
-      height: 300,
-      fonts: [],
-    })
-
-    expect(svg).toMatchSnapshot()
+test('match snapshot', async () => {
+  const App = defineComponent({
+    data: () => ({ width: 300, height: 300, src: 'https://og-image.vercel.app/Hello%20World.png' }),
+    template: '<img :src="src" :width="width" :height="height" />',
   })
+
+  const string = await html(App)
+  const svg = await satori(string, {
+    width: 300,
+    height: 300,
+    fonts: [],
+  })
+
+  expect(svg).toMatchSnapshot()
 })
