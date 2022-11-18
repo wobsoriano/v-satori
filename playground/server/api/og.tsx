c0759423e6676365a990ecafa10c08da9a2d85b2
wobsoriano <sorianorobertc@gmail.com>
2022-11-17 20:27:51 -0800

    test font location

--- playground/server/api/og.tsx
+++ playground/server/api/og.tsx
@@ -15,7 +15,8 @@ async function initFonts(): Promise<SatoriOptions['fonts']> {
     console.log('process.cwd()', process.cwd())
     console.log('fontDataTest', fontDataTest)
   } catch (err) {
-    console.log('process.cwd()', process.cwd())
+    const folders = await fs.readdir(process.cwd(), {withFileTypes: true})
+    console.log('process.cwd()', folders)
     console.log('ERR', err)
   }
 
