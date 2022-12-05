28ced66d10a92c58a96970f66fed273be2fb13a5
wobsoriano <sorianorobertc@gmail.com>
2022-12-05 09:59:50 -0800

    nuxt: remove jsx support. just use SFCs

--- nuxt/module.mjs
+++ nuxt/module.mjs
@@ -6,21 +6,12 @@ export default defineNuxtModule({
     name: 'v-satori',
     configKey: 'satori',
   },
-  defaults: {
-    jsx: false,
-
-  },
   setup(options, nuxt) {
     nuxt.hook('nitro:config', async (config) => {
       config.rollupConfig = config.rollupConfig || {}
       config.rollupConfig.plugins = config.rollupConfig.plugins || []
 
       config.rollupConfig.plugins.push(vue())
-
-      if (options.jsx) {
-        const vueJsx = await import('@vitejs/plugin-vue-jsx').then(r => r.default || r)
-        config.rollupConfig.plugins.push(vueJsx())
-      }
     })
   },
 })
